af0670905a615988180b6be4ed56c5a5ad70673d
connor <connormullett@gmail.com>
2021-04-04 00:17:10 -0400

    read_line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io::Result;
+use std::io::{self, Read, Write};
+
+fn read_line() -> Result<String> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    Ok(buffer)
+}
+
+fn split_line(line: String) -> Vec<String> {
+    Vec::new()
+}
+
+fn execute(args: Vec<String>) -> i32 {
+    0
+}
+
+fn shell_loop() {
+    loop {
+        print!("> ");
+        let _ = io::stdout().flush();
+        let line = read_line().unwrap();
+        println!("{:?}", line);
+        let args = split_line(line);
+        let _status = execute(args);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    shell_loop();
 }
